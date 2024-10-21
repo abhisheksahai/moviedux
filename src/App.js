10fc13bf33ec007326f1478ef047708859eb1a3f
Abhishek Sahai <abhishek.sahai@gartner.com>
2024-10-21 16:56:31 +0530

    moviedux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import "./styles.css";
-import Header from './components/Header';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
         <Header />
+        <Footer />
       </div>
-      <footer className="footer">
-        <p className="footer">Footer content here</p>
-      </footer>
     </div>
   );
 }
